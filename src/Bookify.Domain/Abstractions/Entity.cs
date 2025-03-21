e789186fef4d7938fa620c4b41871188a289c681
qiuxan <qiuxan@gmail.com>
2025-03-21 22:19:42 +1100

    Setup Event and Entity

--- src/Bookify.Domain/Abstractions/Entity.cs
+++ src/Bookify.Domain/Abstractions/Entity.cs
@@ -2,6 +2,8 @@
 
 public abstract class Entity
 {
+    private readonly List<IDomainEvent> _domainEvents = new();
+
     public Entity(Guid id)
     {
         Id = id;
@@ -10,5 +12,19 @@ public abstract class Entity
 
     public Guid Id { get; init; }
 
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return _domainEvents.ToList();
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
 
 }
--- src/Bookify.Domain/Users/User.cs
+++ src/Bookify.Domain/Users/User.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Users.Events;
 
 namespace Bookify.Domain.Users;
 
@@ -19,6 +20,8 @@ public class User:Entity
     public static User Create( FirstName firstName, LastName lastName, Email email)
     {
         var user = new User(Guid.NewGuid(), firstName, lastName, email);
+        user.RaiseDomainEvent(new UserCreatedDomainEvent(user.Id));
+
         return user;
     }
 
