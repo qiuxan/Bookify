86bfc7aba8562f5f98790cd965d7e10d8cd7c463
qiuxan <qiuxan@gmail.com>
2025-03-22 09:29:09 +1100

    Introduce interfaces for work Apartment and Users

---
+++ src/Bookify.Domain/Users/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.Domain.Users;
+
+public interface IUserRepository
+{
+    Task<User?> GetById(Guid id, CancellationToken cancellationToken = default);
+
+    void Add(User user);
+}
