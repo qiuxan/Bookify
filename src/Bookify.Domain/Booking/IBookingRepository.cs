d1e2071fa51b7a88f4b36bf770aa5622ee2d8139
qiuxan <qiuxan@gmail.com>
2025-03-24 22:15:22 +1100

    28 Implementing Commands with the rich domain model & repositories 4:42

---
+++ src/Bookify.Domain/Booking/IBookingRepository.cs
@@ -0,0 +1,16 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Booking;
+
+namespace Bookify.Domain.Booking;
+
+public interface IBookingRepository
+{
+    Task<Booking?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task<bool> IsOverlappingAsync(
+        Apartment apartment,
+        DateRange duration,
+        CancellationToken cancellationToken = default);
+
+    void Add(Booking booking);
+}
