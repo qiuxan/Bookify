060bbe399feb44ecfed9ae2dac9fd1bc5cef34c3
qiuxan <qiuxan@gmail.com>
2025-03-29 20:48:56 +1100

    Configuring infrastructure services with dependency injection

---
+++ src/Bookify.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Email;
+using Bookify.Infrastructure.Clock;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookify.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        services.AddTransient<IEmailService, IEmailService>();
+
+        return services;
+    }
+
+}
---
+++ src/Bookify.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Application.Abstractions.Email;
+
+namespace Bookify.Infrastructure.Email;
+
+internal sealed class EmailService: IEmailService   
+{
+    public Task SendEmailAsync(Domain.Users.Email recipient, string subject, string message)
+    {
+        
+        return Task.CompletedTask;
+    }
+}
