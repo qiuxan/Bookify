843925414aaa20c8a57e22d497a014452c2d1fc6
qiuxan <qiuxan@gmail.com>
2025-03-30 21:27:31 +1100

    Automatically applying entity configurations

---
+++ src/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure;
+
+public class ApplicationDbContext: DbContext,IUnitOfWork
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+
+}
