3006e1b59262bcf0e30e5967bf90f9bbda1f4bd8
qiuxan <qiuxan@gmail.com>
2025-04-03 21:07:07 +1100

    Add onModel Creating override function

--- src/Bookify.Infrastructure/ApplicationDbcontext.cs
+++ src/Bookify.Infrastructure/ApplicationDbcontext.cs
@@ -10,5 +10,12 @@ public class ApplicationDbContext: DbContext,IUnitOfWork
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
 
 }
